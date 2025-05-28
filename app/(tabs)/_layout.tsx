00cc1d78b8351615f29f7f0ec06349d91ff12003
Kandy-Hamisi <hkandy2000@gmail.com>
2025-05-28 19:30:55 +0300

    Implemented tab styling and fixed routing

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,67 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const TabsLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        headerStyle: { backgroundColor: "#f5f5f5" },
+        headerShadowVisible: false,
+        tabBarStyle: {
+          backgroundColor: "#f5f5f5",
+          borderTopWidth: 0,
+          elevation: 0,
+          shadowOpacity: 0,
+        },
+        tabBarActiveTintColor: "#6200ee",
+        tabBarInactiveTintColor: "#666666",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ color, focused, size }) => (
+            <MaterialCommunityIcons
+              name="calendar-today"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="streaks"
+        options={{
+          title: "Streaks",
+          headerShown: false,
+          tabBarIcon: ({ color, focused, size }) => (
+            <MaterialCommunityIcons
+              name="chart-line"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="add-habit"
+        options={{
+          title: "Add Habit",
+          headerShown: false,
+          tabBarIcon: ({ color, focused, size }) => (
+            <MaterialCommunityIcons
+              name="plus-circle"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+export default TabsLayout;
---
+++ app/(tabs)/add-habit.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const AddHabitPage = () => {
+  return (
+    <View>
+      <Text>AddHabitPage</Text>
+    </View>
+  );
+};
+export default AddHabitPage;
